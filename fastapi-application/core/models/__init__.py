4b51d3e28a04ae66acf001bbf9714f77ad278e2e
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-07-18 03:52:29 +0800

    add access_token

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -1,9 +1,6 @@
-__ALL__ = (
-    "Base",
-    "db_helper",
-    "User"
-)
+__ALL__ = ("Base", "db_helper", "User", "AccessToken")
 
 from .base import Base
 from .db_helper import db_helper
 from .user import User
+from .access_token import AccessToken
--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,9 +1,19 @@
-from fastapi_users.db import SQLAlchemyBaseUserTable
+from typing import TYPE_CHECKING
 
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase,
+)
 
 from core.models import Base
 from core.models.mixins.id_int_pk import IdIntPkMixin
+from core.models.types.user_id import UserIdType
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
-    pass
+class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, cls)
