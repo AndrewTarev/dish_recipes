dfc9067cc4f4b14bc343235d03c9243c946f768c
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-07-18 02:48:22 +0800

    add user

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,34 +1,9 @@
-from typing import AsyncGenerator
-
-from fastapi import Depends
-from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
-
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+from fastapi_users.db import SQLAlchemyBaseUserTable
 
 
-class Base(DeclarativeBase):
+from core.models import Base
+from core.models.mixins.id_int_pk import IdIntPkMixin
+
+
+class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
     pass
-
-
-class User(SQLAlchemyBaseUserTableUUID, Base):
-    pass
-
-
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def create_db_and_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
-        yield session
-
-
-async def get_user_db(session: AsyncSession = Depends(get_async_session)):
-    yield SQLAlchemyUserDatabase(session, User)
