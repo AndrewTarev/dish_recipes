97539884b5b3f165ae0b2178d403846cb30d0292
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-07-18 02:45:04 +0800

    do good config

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel
-from pydantic import PostgresDsn
 from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
@@ -22,22 +21,24 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
     v1: ApiV1Prefix = ApiV1Prefix()
 
-    @property
-    def bearer_token_url(self) -> str:
-        # api/v1/auth/login
-        parts = (self.prefix, self.v1.prefix, self.v1.auth, "/login")
-        path = "".join(parts)
-        # return path[1:]
-        return path.removeprefix("/")
 
+class DatabaseConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+    )
 
-class DatabaseConfig(BaseModel):
-    url: PostgresDsn
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_NAME: str
+
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 50
     max_overflow: int = 10
 
+    # авто наминг для ключей БД алембик
     naming_convention: dict[str, str] = {
         "ix": "ix_%(column_0_label)s",
         "uq": "uq_%(table_name)s_%(column_0_N_name)s",
@@ -46,24 +47,15 @@ class DatabaseConfig(BaseModel):
         "pk": "pk_%(table_name)s",
     }
 
-
-class AccessToken(BaseModel):
-    lifetime_seconds: int = 3600
-    reset_password_token_secret: str
-    verification_token_secret: str
+    @property
+    def url(self):
+        return f"postgresql+asyncpg://{self.postgres_user}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_NAME}"
 
 
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=(".env.template", ".env"),
-        case_sensitive=False,
-        env_nested_delimiter="__",
-        env_prefix="APP_CONFIG__",
-    )
+class Settings(BaseModel):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
-    db: DatabaseConfig
-    access_token: AccessToken
+    db: DatabaseConfig = DatabaseConfig()
 
 
 settings = Settings()
