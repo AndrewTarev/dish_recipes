649d122b6e15495a4a90a608e2ab75e740f3fbab
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-08-05 21:19:23 +0200

    add test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,9 +1,9 @@
 # python -m pytest -v
+from os import getenv
 
 from httpx import AsyncClient
 import pytest
 
-from fastapi_application.actions.create_superuser import create_superuser
 from tests.conftest import async_session
 from tests.settings_test import UserTest, login_credentials_schema
 
@@ -27,22 +27,43 @@ async def test_login_user(ac: AsyncClient) -> None:
         "/api/v1/auth/login",
         data=login_credentials_schema,
     )
+    assert response.json().get("access_token") is not None
+    assert response.json().get("token_type") == "bearer"
     assert response.status_code == 200
 
 
 @pytest.mark.asyncio
-async def test_logout_user(ac: AsyncClient) -> None:
-    response = await ac.post("/api/v1/auth/logout")
-    assert response.status_code == 200
+async def test_request_verify_token_user(ac: AsyncClient) -> None:
+    response = await ac.post(
+        "/api/v1/auth/request-verify-token",
+        json={"email": UserTest.EMAIL},
+    )
+    assert response.status_code == 202
 
 
 @pytest.mark.asyncio
 async def test_create_superuser(ac: AsyncClient) -> None:
-    await create_superuser(async_session)
+    superuser = await create_superuser(async_session)
+    assert superuser.email == getenv("DEFAULT_EMAIL")
+    assert superuser.is_superuser is True
 
 
-# @pytest.mark.asyncio
-# async def test_create_dishes(ac: AsyncClient) -> None:
-#     response = await ac.post(
-#         "/api/v1/auth/create_dishes",
-#     )
+@pytest.mark.asyncio
+async def test_reset_password_token(
+    ac: AsyncClient,
+) -> None:
+    login = await ac.post("/api/v1/auth/forgot-password", json=UserTest.EMAIL)
+    login_token = login.headers.get("Authorization")
+    if not login_token:
+        # Если токен в заголовках отсутствует, проверяем тело ответа
+        login_json = login.json()
+        login_token = login_json.get("token")
+
+    response = await ac.post(
+        "/api/v1/auth/reset-password",
+        json={
+            "token": login_token,
+            "password": UserTest.NEW_PASSWORD,
+        },
+    )
+    assert response.status_code == 200
